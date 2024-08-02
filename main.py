20dd1c87649a2d7d442a9e7eb96cbbd724bc27dd
linh <linh.ha.nguyen20@gmail.com>
2024-08-02 12:45:43 -0700

    init cluster in fastapi

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+import os
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
+from couchbase.cluster import Cluster
+from couchbase.auth import PasswordAuthenticator
+
+load_dotenv()
 
 app = FastAPI()
 
 
+def get_cluster():
+    return Cluster("couchbase://localhost",
+                  authenticator=PasswordAuthenticator(os.environ.get("USERNAME"), os.environ.get("PASSWORD")))
+
+
+def get_inventory_collection():
+    collection = get_cluster().bucket("personalized-blogs").scope("inventory")
+    return collection
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello world'}
